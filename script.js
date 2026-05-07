18ceebc115e0675399b207b9a87d5e9ccd66ac63
DogShown <rm99877@estudante.fieb.edu.br>
2026-05-07 16:27:16 -0300

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById('trail-canvas');
+const canvas = document.getElementById('trailCanvas');
 const ctx = canvas.getContext('2d');
 let particles = [];
 
@@ -14,21 +14,17 @@ class Particle {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.size = Math.random() * 3 + 1;
-        this.speedX = Math.random() * 1 - 0.5;
-        this.speedY = Math.random() * 1 - 0.5;
-        this.color = '#c5a059'; // Dourado suave
-        this.opacity = 1;
+        this.size = Math.random() * 5 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+        this.color = '#deb887';
     }
-
     update() {
         this.x += this.speedX;
         this.y += this.speedY;
-        this.opacity -= 0.02;
+        if (this.size > 0.2) this.size -= 0.1;
     }
-
     draw() {
-        ctx.globalAlpha = this.opacity;
         ctx.fillStyle = this.color;
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
@@ -47,12 +43,24 @@ function animate() {
     for (let i = 0; i < particles.length; i++) {
         particles[i].update();
         particles[i].draw();
-        if (particles[i].opacity <= 0) {
+        if (particles[i].size <= 0.3) {
             particles.splice(i, 1);
             i--;
         }
     }
     requestAnimationFrame(animate);
+    // Criar o objeto de áudio
+const somClique = new Audio('click.mp3');
+
+// Função para tocar o som
+function tocarSom() {
+    somClique.currentTime = 0; // Reinicia o som se clicar rápido
+    somClique.play();
 }
 
+// Exemplo: Aplicar a todos os botões do seu site
+document.querySelectorAll('button').forEach(botao => {
+    botao.addEventListener('click', tocarSom);
+});
+}
 animate();
